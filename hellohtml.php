7bd6b2a10ac0cfd57fa31aacc63f29818604f521
Pavel Shavshukov <smart_screen@outlook.com>
2020-03-24 13:48:59 +0300

    Create hellohtml.php

---
+++ hellohtml.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+<title>PHP Hello Example</title>
+</head>
+
+<body>
+
+<h1>PHP Hello Example</h1>
+
+<p>
+<?php
+    echo "Hello\n";
+    echo "From the PHP Server\n";
+?>
+</p>
+
+</body>
+
+</html>
